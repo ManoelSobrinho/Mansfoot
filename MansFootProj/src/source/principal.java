1589628d658129599baf7d74bbb6f57d80825ad1
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2020-04-22 08:26:45 -0300

    Projeto inicial

---
+++ MansFootProj/src/source/principal.java
@@ -0,0 +1,23 @@
+package source;
+
+
+        
+/**
+ *
+ * @author Manoel Cleonaldo
+ */
+public class principal {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("Oi\n");
+        
+        clube Flamengo = new clube();
+        
+        Flamengo.setNome("Flamengo");
+        
+        Flamengo.showNome();
+        
+    }
+    
+}
---
+++ MansFootProj/src/source/tecnico.java
@@ -0,0 +1,27 @@
+package source;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Manoel Cleonaldo
+ */
+public class tecnico {
+    
+    private int id;
+    
+    private String nome;
+    private String clube;
+    private double salario;
+    private String nacionalidade;
+    private int idade;
+    private String titulos[];
+    private String clubesQueTreinou[];
+    
+    
+}
